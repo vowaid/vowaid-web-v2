0f6f88b0bdcb1398ca6998a1ed04764ab712ab04
Holly Springsteen <hhspringsteen@gmail.com>
2020-03-15 23:36:24 -0500

    add catch for textarea w/ note for later

--- src/utils/enums/formEnums.js
+++ src/utils/enums/formEnums.js
@@ -1,30 +1,32 @@
 // import { Field } from 'formik';
 import { TextField } from '@material-ui/core';
-import { Select } from '../../index';
+import { Select } from '../../components';
 
 const FieldTypes = {
   TEXT: 'text',
   EMAIL: 'email',
   TEL: 'tel',
+  SELECT: 'select',
+  TEXTAREA: 'textarea',
 };
 
 const formFields = {
-  // Field - Sub-Components
-  Name: {
+  // Field - Input
+  name: {
     initialValue: '',
     component: TextField,
     name: 'name',
     label: 'Name',
     type: FieldTypes.TEXT,
   },
-  EMAIL: {
+  email: {
     initialValue: '',
     component: TextField,
     name: 'email',
     label: 'Email',
     type: FieldTypes.EMAIL,
   },
-  PHONE: {
+  phone: {
     initialValue: '',
     component: TextField,
     name: 'phone',
@@ -33,37 +35,53 @@ const formFields = {
   },
 
   // Select
-  BRANCH: {
+  branch: {
     initialValue: '',
     component: Select,
     name: 'branch',
     label: 'Branch',
+    type: FieldTypes.SELECT,
   },
-  RANK: {
+  rank: {
     initialValue: '',
     component: Select,
     name: 'rank',
     label: 'Rank',
+    type: FieldTypes.SELECT,
   },
-  DISCHARGE: {
+  discharge: {
     initialValue: '',
     component: Select,
     name: 'discharge',
     label: 'Discharge',
+    type: FieldTypes.SELECT,
   },
 
-  // Textarea
-  MESSAGE: {
+  // Field - Textarea
+  message: {
     initialValue: '',
-    component: TextArea,
+    component: null,
     name: 'message',
     label: 'Message',
+    type: FieldTypes.TEXTAREA,
     props: {
       multiline: true,
     },
   },
 };
 
+const contactFormValues = {
+  'name': '',
+  'email': '',
+  'phone': '',
+  'branch': '',
+  'rank': '',
+  'discharge': '',
+  'message': '',
+};
+
 export {
+  contactFormValues,
+  FieldTypes,
   formFields,
 };
