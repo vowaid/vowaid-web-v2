40a7e3735e46f055b295b59f46e0c4b722a326ab
Holly Springsteen <hhspringsteen@gmail.com>
2020-03-16 19:45:20 -0500

    fix formik tests

--- src/components/forms/DatePicker/DatePicker.test.js
+++ src/components/forms/DatePicker/DatePicker.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
+import { Formik } from 'formik';
 import DatePicker from './DatePicker';
 
 describe('DatePicker Tests', () => {
   it('should render without errors with default props', () => {
-    render(<DatePicker />);
+    render(
+      <Formik>
+        <DatePicker />
+      </Formik>
+    );
   });
 });
--- src/components/forms/Password/Password.test.js
+++ src/components/forms/Password/Password.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
+import { Formik } from 'formik';
 import Password from './Password';
 
 describe('Password Tests', () => {
   it('should render without errors with default props', () => {
-    render(<Password />);
+    render(
+      <Formik>
+        <Password />
+      </Formik>
+    );
   });
 });
