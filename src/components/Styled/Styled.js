fb942d5c80539c2e789ac9351fcc3cf5ae181ea3
Holly Springsteen <hhspringsteen@gmail.com>
2020-03-19 23:32:38 -0500

    fix padding removal from Wrapper

--- src/components/Styled/Styled.js
+++ src/components/Styled/Styled.js
@@ -3,15 +3,18 @@ import styled from 'styled-components';
 import Content from '../Content/Content';
 
 export const Wrapper = styled(Content)`
-  padding: 0 15vw;
+  padding-left: 15vw;
+  padding-right: 15vw;
   text-align: left;
   width: 100vw;
 
   @media only screen and (max-width: 800px) {
-    padding: 0 10vw;
+    padding-left: 10vw;
+    padding-right: 10vw;
   }
 
   @media only screen and (max-width: 600px) {
-    padding: 0 5vw;
+    padding-left: 5vw;
+    padding-right: 5vw;
   }
 `;
