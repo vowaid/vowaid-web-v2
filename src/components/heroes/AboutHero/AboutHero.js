937848335b5b54f21ad6677dbeff39ddb498011a
Holly Springsteen <hhspringsteen@gmail.com>
2020-05-04 23:58:24 -0500

    translate y only (mobile issues)

--- src/components/heroes/AboutHero/AboutHero.js
+++ src/components/heroes/AboutHero/AboutHero.js
@@ -23,7 +23,7 @@ const Hero = () => {
       const scale = window.scrollY * .0001 + 1;
       const translate = (window.scrollY * .2 + 1) * -1; // Title speed
 
-      document.querySelector('.hero--mission').style.transform = `scale(${scale}) translate(${translate}px, ${translate}px)`;
+      document.querySelector('.hero--mission').style.transform = `scale(${scale}) translateY(${translate}px)`;
     });
   });
 
