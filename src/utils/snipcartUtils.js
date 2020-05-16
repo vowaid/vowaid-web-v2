846c45c63d1599c77a14ff32f408506e0b7b71c0
Holly Springsteen <hhspringsteen@gmail.com>
2020-05-16 13:51:14 -0500

    fix id generator

--- src/utils/snipcartUtils.js
+++ src/utils/snipcartUtils.js
@@ -1,12 +1,13 @@
+// Generate an id that has 4 digits
 const generateDonationId = (amount) => {
-  let id = amount;
+  let id = amount.toString();
 
-  if (amount.length === 3) {
-    id = `0${amount}`;
-  } else if (amount.length === 2) {
-    id = `00${amount}`;
-  } else if (amount.length === 1) {
-    id = `000${amount}`;
+  if (id.length === 3) {
+    id = `0${id}`;
+  } else if (id.length === 2) {
+    id = `00${id}`;
+  } else if (id.length === 1) {
+    id = `000${id}`;
   }
 
   id += 'D';
